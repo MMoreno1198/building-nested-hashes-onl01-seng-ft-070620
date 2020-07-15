16d6c7955d8ad9e1957158be98e3f47d729adc16
Marcus Moreno <legendary.1198@gmail.com>
2020-07-15 19:33:53 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,19 @@
 def first_challenge
+  
+ def first_challenge
+   
    epic_tragedy = {}
+   
+end 
+
+  epic_tragedy = {
+    
+    :montague => {
+      
+       },
+    
+    :capulet => {
+      
+      }}
+end
 end
